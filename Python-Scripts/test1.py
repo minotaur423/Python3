642ebbaafb93c2483ad4061cc6dc13fb974e63c4
minotaur423 <minotaur423@me.com>
2019-09-12 15:50:42 -0400

    Completed CodeAbbey problem 22, started 23.

--- Python-Scripts/test1.py
+++ Python-Scripts/test1.py
@@ -1,16 +1,37 @@
 # Problem 22
 def two_prnt(x,y,n):
     time = 0
+    pages = 0
+    f_time = 0
+    s_time = 0
+    faster = 0
+    slower = 0
+    max_time = 0
     if x < y:
         faster = x
         slower = y
-    else:
+        max_time = slower * n
+    elif y < x:
         faster = y
         slower = x
-    difference = slower - faster
-    for i in range(1, n+1, faster):
-        time += x*n + y*n)
-    time = time/n
+        max_time = slower * n
+    else:
+        slower = x
+        faster = y
+        max_time = (slower + 1) * n
+    for i in range(1, max_time):
+        if pages >= n:
+            time = max(f_time,s_time)
+            break
+        else:
+            if i % faster == 0:
+                pages += 1
+                f_time += faster
+            if i % slower == 0:
+                pages += 1
+                s_time += slower
     return time
 
-print(two_prnt(3, 5, 4), end=" ")
+grps = ([105684995, 96944250, 8], [12, 88, 10647638], [1, 1, 927175744], [4979224, 7901223, 94], [388996, 376949, 772], [90334, 23099, 5050], [705, 2744, 294119], [68, 3504, 215562], [6, 8, 85993693], [1, 1, 658189901], [760, 995, 749854], [1288, 2455, 379320], [3, 2, 78874497], [407746, 1383237, 680], [2, 2, 484393235], [52, 73, 7621777])
+for a,b,c in grps:
+    print(two_prnt(a,b,c), end=" ")
--- Python-Scripts/test2.py
+++ Python-Scripts/test2.py
@@ -1,7 +1,10 @@
-data = (3, 1, 4, 1, 5, 9)
-modulo = 10000007
-multiplier = 113
-result = 0
-for i in data:
-    result = (result + i) * multiplier
-print(result  % modulo)
+# Problem 23
+def array_chksm(*args):
+    result = 0
+    value = 113
+    modulo = 10000007
+    for i in range(len(args)):
+        result += (result + args[i]) * value
+    return (result % modulo)
+
+print(array_chksm(3, 1, 4, 1, 5, 9))
