3762a9b6a6372c7d1c3e96f4d93a497d2dbe9333
minotaur423 <minotaur423@me.com>
2019-09-13 15:42:06 -0400

    Practiced Python today.

--- Python-Scripts/test1.py
+++ Python-Scripts/test1.py
@@ -1,37 +1,13 @@
-# Problem 22
-def two_prnt(x,y,n):
-    time = 0
-    pages = 0
-    f_time = 0
-    s_time = 0
-    faster = 0
-    slower = 0
-    max_time = 0
-    if x < y:
-        faster = x
-        slower = y
-        max_time = slower * n
-    elif y < x:
-        faster = y
-        slower = x
-        max_time = slower * n
-    else:
-        slower = x
-        faster = y
-        max_time = (slower + 1) * n
-    for i in range(1, max_time):
-        if pages >= n:
-            time = max(f_time,s_time)
-            break
-        else:
-            if i % faster == 0:
-                pages += 1
-                f_time += faster
-            if i % slower == 0:
-                pages += 1
-                s_time += slower
-    return time
+# Problem 23
+import string
 
-grps = ([105684995, 96944250, 8], [12, 88, 10647638], [1, 1, 927175744], [4979224, 7901223, 94], [388996, 376949, 772], [90334, 23099, 5050], [705, 2744, 294119], [68, 3504, 215562], [6, 8, 85993693], [1, 1, 658189901], [760, 995, 749854], [1288, 2455, 379320], [3, 2, 78874497], [407746, 1383237, 680], [2, 2, 484393235], [52, 73, 7621777])
-for a,b,c in grps:
-    print(two_prnt(a,b,c), end=" ")
+def ispangram(str1, alphabet=string.ascii_lowercase):
+    for i in alphabet:
+        if i in str1.lower():
+            result = True
+        else:
+            result = False
+            break
+    return result
+
+print(ispangram("The quick brown fox jumps over lazy dog"))
--- Python-Scripts/test2.py
+++ Python-Scripts/test2.py
@@ -1,5 +1,5 @@
 # Problem 23
-def array_chksm(*args):
+def array_chksm(args):
     result = 0
     value = 113
     modulo = 10000007
@@ -7,4 +7,11 @@ def array_chksm(*args):
         result = (((args[i] + result) * value) % modulo)
     return (result)
  
-print(array_chksm(3, 1, 4, 1, 5, 9))
+data = [1, 4, 3, 2, 6, 5, -1]
+for i in range(len(data)-1):
+    if data[i] > data[i+1] and data[i+1] != -1:
+        data[i], data[i+1] = data[i+1], data[i]
+    else:
+        continue
+data.pop()
+print(array_chksm(data))
