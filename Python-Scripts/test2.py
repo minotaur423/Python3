e81d0f298f45e9b3e7e025bb1bcd76fc5959f660
Jose C Santos <minotaur423@comcast.net>
2019-09-12 22:15:35 -0400

    Update test2.py

--- Python-Scripts/test2.py
+++ Python-Scripts/test2.py
@@ -4,7 +4,7 @@ def array_chksm(*args):
     value = 113
     modulo = 10000007
     for i in range(len(args)):
-        result += (result + args[i]) * value
-    return (result % modulo)
-
+        result = (((args[i] + result) * value) % modulo)
+    return (result)
+ 
 print(array_chksm(3, 1, 4, 1, 5, 9))
