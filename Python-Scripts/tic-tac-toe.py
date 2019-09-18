64b2753f3e12acbe15a6e832db5e62485fbf45a2
minotaur423 <minotaur423@me.com>
2019-09-17 21:36:16 -0400

    Finished the Python3 Milestone Project.

--- Python-Scripts/tic-tac-toe.py
+++ Python-Scripts/tic-tac-toe.py
@@ -1,6 +1,9 @@
 # Function that prints out a board
 # from IPython.display import clear_output
+
 def display_board(board):
+#    clear_output()  # Remember, this only works in jupyter!
+    
     print(f'      |       |      ')
     print(f'  {board[7]}   |   {board[8]}   |   {board[9]}  ')
     print(f'      |       |      ')
@@ -14,16 +17,25 @@ def display_board(board):
     print(f'      |       |      ')
 
 # Function takes in player input and assigns marker as 'X' or 'O'
-def player_input():
+def player_input(player):
     marker = ''
     while not marker:
-        player1 = input("Please pick a marker 'X' or 'O': ")
-        if player1.lower() == 'x':
+        marker = input("Please pick a marker 'X' or 'O': ")
+        if player == 1 and marker.lower() == 'x':
             marker = 'X'
-            print(f'Player1 is {marker} and Player2 is O.')
-        else:
+            print('\n' + f'Player1 is X and Player2 is O.')
+        elif player == 1 and marker.lower() == 'o':
             marker = 'O'
-            print(f'Player1 is {marker} and Player2 is X.')
+            print('\n' + f'Player1 is O and Player2 is X.')
+        elif player == 2 and marker.lower() == 'x':
+            marker = 'X'
+            print('\n' + f'Player2 is X and Player1 is O.')
+        elif player == 2 and marker.lower() == 'o':
+            marker = 'O'
+            print('\n' + f'Player2 is O and Player1 is X.')
+        else:
+            print('\n' + f'Your selection "{marker}" is not valid.\n')
+            marker = ''
     return marker
 
 # Function takes in the board list object, a marker ('X' or 'O'), and a desired position (number 1-9) and assigns it to the board.
@@ -62,9 +74,10 @@ def full_board_check(board):
         if board[i] == " ":
             result = False
     return result
+
 # Function asks player if they want to play again; returns a boolean True if yes.
 def replay():
-    restart = input('Would you like to play again? (Y/n)')
+    restart = input('\nWould you like to play again? (Y/n)')
     if restart.lower() == 'y':
         return True
     else:
@@ -72,12 +85,16 @@ def replay():
 
 # Function asks for player's next position and then uses function from step 6 to check if it's a free position. If it is, then return the position for later use.
 def player_choice(board):
-    position = int(input('Please enter a number: '))
-    if space_check(board,position) == True:
-        return position
-    else:
-        return 'Space used.'
+    position = ''
+    while not position:
+        position = int(input('\nPlease enter a number: '))
+        if space_check(board,position) == True:
+            return position
+        else:
+            print(f'The number {position} has been used. Pick another number.')
+            position = ''
 
+# Function to select the first player.
 from random import randint
 
 def choose_first():
@@ -98,27 +115,33 @@ while True:
     if first_player == 1:
         second_player = 2
         print(f'Player1 will go first.'+'\n')
+        # This function will return a marker for the first player.
+        marker_selected = player_input(first_player)
+        if marker_selected == 'X':
+            first = 'X'
+            second = 'O'
+        else:
+            second = 'X'
+            first = 'O'
     else:
-        first_player == 2
         second_player = 1
         print(f'Player2 will go first.'+'\n')
-
-    # This function will return a marker for the first player.
-    marker_selected = player_input()
-    if marker_selected == 'X':
-        first = 'X'
-        second = 'O'
-    else:
-        second = 'X'
-        first = 'O'
+        # This function will return a marker for the first player.
+        marker_selected = player_input(first_player)
+        if marker_selected == 'X':
+            first = 'X'
+            second = 'O'
+        else:
+            second = 'X'
+            first = 'O'
 
     while game_on:
         #Player 1 Turn
-        slot = int(input('Please enter a number: '))
+        slot = int(player_choice(test_board))
         place_marker(test_board,first,slot)
         display_board(test_board)
         if  win_check(test_board, first) == True:
-            print(f'Player{first_player} wins!!!')
+            print('\n' + f'Player{first_player} wins!!!')
             game_on = False
             break
         elif full_board_check(test_board) == True:
@@ -127,11 +150,11 @@ while True:
             break
         
         # Player2's turn.
-        slot = int(input('Please enter a number: '))
+        slot = int(player_choice(test_board))
         place_marker(test_board,second,slot)
         display_board(test_board)
-        if  win_check(test_board, first) == True:
-            print(f'Player{second_player} wins!!!')
+        if  win_check(test_board, second) == True:
+            print('\n' + f'Player{second_player} wins!!!')
             game_on = False
             break
         elif full_board_check(test_board) == True:
