d1631c6b86a6228b7b69ff840dfc34e1ab16b9fe
minotaur423 <minotaur423@me.com>
2019-10-03 21:47:28 -0400

    Completed the Bubble Sort problem.

--- Python-Scripts/bubble_sort.py
+++ Python-Scripts/bubble_sort.py
@@ -1,11 +1,25 @@
 def bubble_sort(data):
-    new_data = data
-    for i in range(len(new_data)):
-        if new_data[i] <= new_data[i + 1]:
-            continue
-        else:
-            new_data[i] = new_data[i + 1]
-    return new_data
+    swaps = 1
+    passes = 0
+    swap_total = 0
+    n1 = 0
+    n2 = 1
+    while swaps:
+        n1 = 0
+        n2 = 1
+        swaps = 0
+        for i in range(len(data) - 1):
+            if data[n1] <= data[n2]:
+                pass
+            else:
+                data[n1], data[n2] = data[n2], data[n1]
+                swaps += 1
+            n1 += 1
+            n2 += 1
+        swap_total += swaps
+        passes += 1
+    result = (f'{passes} {swap_total}')  
+    return result
         
-a = [3, 1, 4, 1, 5, 9, 2, 6]
-print(f'The original list {a} sorted:  {bubble_sort(a)}.')
+a = [6, 16, 9, 12, 17, 1, 3, 2, 14, 13, 8, 4, 5, 11, 10, 15, 7]
+print(f'{bubble_sort(a)}')
