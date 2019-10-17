78e4061fe081fdec1db9609f26ab6869cf0a6f0b
minotaur423 <minotaur423@me.com>
2019-10-17 14:55:51 -0400

    Tried to complete bin search problem.

--- Python-Scripts/bin_search-1.py
+++ Python-Scripts/bin_search-1.py
@@ -1,10 +1,28 @@
 data = ([0.59912051, 0.64030348, 263.33721367, 387.92069617], [15.68387514, 1.26222280, 695.23706506, 698.72384731])
 from math import sqrt,exp
-x = 0
+from decimal import *
+from numpy import arange
+
+x = 0.0
 for a, b, c, d in data:
-    for x in range(1000):
-        result = a * x + b * (x**(3/2)) - c * (exp(-x/50)) - d
+    for x in arange(0.000000001, 1000, 0.00001):
+        result = a * x + b * sqrt(x**3) - c * (exp(-x/50)) - d
         if result >= 0:
             break
         x += 1
-    print(round(x/1,12), end=" ")
+    getcontext().prec = 12
+    print(x, end=" ")
+    
+print("Float range using NumPy arange():")
+
+print("\nTest 1:")
+for i in arange(0.0, 1.0, 0.1):
+    print(i, end=', ')
+
+print("\n\nTest 2:")
+for i in arange(0.5, 5.5, 0.5):
+    print(i, end=', ')
+
+print("\n\nTest 3:")
+for i in arange(-1.0, 1.0, 0.5):
+    print(i, end=', ')
