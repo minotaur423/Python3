05902c53e4c56ab89711b98a1f5fa3f3589a6ba6
minotaur423 <minotaur423@me.com>
2019-11-06 15:28:07 -0500

    Completed exercise 35.

--- Python-Scripts/savings_calc.py
+++ Python-Scripts/savings_calc.py
@@ -1,14 +1,18 @@
+import math
 def calc_savings(s, r, p):
     new_s = s
-    for y in range(1,31):
-        new_s += (new_s * (p/100))
-        if new_s >= r:
+    y = 0
+    while True:
+        new_s += (math.floor(new_s) * (p/100))
+        if new_s > r:
+            y += 1
             break
         else:
+            y += 1
             continue
     return y
 
-data=([1000, 10000, 8], [50, 100, 25])
+data=([50, 750, 7], [25, 475, 25], [5000, 70000, 35], [500, 8500, 3], [500, 9000, 1], [500, 4000, 35], [100, 2000, 25], [1000, 5000, 50], [1000, 12000, 7], [25, 375, 50], [250, 750, 1], [250, 750, 7], [100, 800, 40], [2500, 12500, 25], [250, 1250, 7], [2500, 17500, 5], [100, 500, 10])
 
 for a,b,c in data:
     print(calc_savings(a,b,c), end=" ")
